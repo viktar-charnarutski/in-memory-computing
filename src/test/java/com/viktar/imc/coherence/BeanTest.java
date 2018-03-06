b404346ac1e1901828d68809532a1ad329cf5b79
viktar-charnarutski <v.charnarutski@gmail.com>
2018-03-06 08:36:59 +0200

    An abstract class for basic junit tests required for POJOs.

---
+++ src/test/java/com/viktar/imc/coherence/BeanTest.java
@@ -0,0 +1,86 @@
+package com.viktar.imc.coherence;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public abstract class BeanTest<T> extends MockTest {
+
+    protected abstract T getEmptyBeanInstance();
+
+    protected abstract T getPopulatedBeanInstance();
+
+    protected abstract T getUniquePopulatedBeanInstance();
+
+    protected List<T> getAllVarietiesOfTheBeanInstance() {
+        List<T> beans = new ArrayList<>();
+        beans.add(getPopulatedBeanInstance());
+        beans.add(getEmptyBeanInstance());
+        return beans;
+    }
+
+    public abstract void toStringMethod();
+
+    @Test
+    public void equalsForEaqualObjects() {
+        assertTrue(getPopulatedBeanInstance().equals(getPopulatedBeanInstance()));
+    }
+
+    @Test
+    public void equalsForNullObject() {
+        assertFalse(getPopulatedBeanInstance().equals(null));
+    }
+
+    @Test
+    public void equalsForDifferentTypeObject() {
+        assertFalse(getPopulatedBeanInstance().equals(new Object()));
+    }
+
+    @Test
+    public void equalsForTheSameObjects() {
+        T beanInstance = getPopulatedBeanInstance();
+        assertTrue(beanInstance.equals(beanInstance));
+    }
+
+    @Test
+    public void equalsForNotEaqualObjects() {
+        T beanInstance = getUniquePopulatedBeanInstance();
+        for (T beanInstanceVaraity : getAllVarietiesOfTheBeanInstance()) {
+            assertFalse(beanInstance.equals(beanInstanceVaraity));
+        }
+    }
+
+    @Test
+    public void hashCodeForEaqualObjects() {
+        T beanInstance1 = getPopulatedBeanInstance();
+        T beanInstance2 = getPopulatedBeanInstance();
+        assertEquals(beanInstance1.hashCode(), beanInstance2.hashCode());
+    }
+
+    @Test
+    public void hashCodeForNotEaqualObjects() {
+        T beanInstance = getUniquePopulatedBeanInstance();
+        for (T beanInstanceVaraity : getAllVarietiesOfTheBeanInstance()) {
+            assertNotEquals(beanInstance.hashCode(), beanInstanceVaraity.hashCode());
+        }
+    }
+
+    @Test
+    public void hashCodeEqualContract() {
+        T beanInstance1 = getPopulatedBeanInstance();
+        T beanInstance2 = getPopulatedBeanInstance();
+
+        assertTrue(beanInstance1.equals(beanInstance2));
+        assertEquals(beanInstance1.hashCode(), beanInstance2.hashCode());
+
+        T beanInstance3 = getUniquePopulatedBeanInstance();
+
+        for (T beanInstanceVaraity : getAllVarietiesOfTheBeanInstance()) {
+            assertFalse(beanInstance3.equals(beanInstanceVaraity));
+            assertNotEquals(beanInstance3.hashCode(), beanInstanceVaraity.hashCode());
+        }
+    }
+}
