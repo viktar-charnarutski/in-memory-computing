7b92da9434ac855e7aee7e06574983b7cb647cbf
viktar-charnarutski <v.charnarutski@gmail.com>
2018-03-08 14:26:42 +0200

    Fixing equals and hashCode functions.

--- src/main/java/com/viktar/imc/coherence/vacplanner/Trip.java
+++ src/main/java/com/viktar/imc/coherence/vacplanner/Trip.java
@@ -90,26 +90,26 @@ public class Trip extends AbstractEvolvable implements EvolvablePortableObject {
         if (this == o) return true;
         if (!(o instanceof Trip)) return false;
 
-        Trip flightHotelBundle = (Trip) o;
+        Trip trip = (Trip) o;
 
-        if (!departure.equals(flightHotelBundle.departure)) return false;
-        if (!destination.equals(flightHotelBundle.destination)) return false;
-        if (!startDate.equals(flightHotelBundle.startDate)) return false;
-        if (!endDate.equals(flightHotelBundle.endDate)) return false;
-        if (!price.equals(flightHotelBundle.price)) return false;
-        if (!hotel.equals(flightHotelBundle.hotel)) return false;
-        return url.equals(flightHotelBundle.url);
+        if (departure != null ? !departure.equals(trip.departure) : trip.departure != null) return false;
+        if (destination != null ? !destination.equals(trip.destination) : trip.destination != null) return false;
+        if (startDate != null ? !startDate.equals(trip.startDate) : trip.startDate != null) return false;
+        if (endDate != null ? !endDate.equals(trip.endDate) : trip.endDate != null) return false;
+        if (price != null ? !price.equals(trip.price) : trip.price != null) return false;
+        if (hotel != null ? !hotel.equals(trip.hotel) : trip.hotel != null) return false;
+        return url != null ? url.equals(trip.url) : trip.url == null;
     }
 
     @Override
     public int hashCode() {
-        int result = departure.hashCode();
-        result = 31 * result + destination.hashCode();
-        result = 31 * result + startDate.hashCode();
-        result = 31 * result + endDate.hashCode();
-        result = 31 * result + price.hashCode();
-        result = 31 * result + hotel.hashCode();
-        result = 31 * result + url.hashCode();
+        int result = departure != null ? departure.hashCode() : 0;
+        result = 31 * result + (destination != null ? destination.hashCode() : 0);
+        result = 31 * result + (startDate != null ? startDate.hashCode() : 0);
+        result = 31 * result + (endDate != null ? endDate.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (hotel != null ? hotel.hashCode() : 0);
+        result = 31 * result + (url != null ? url.hashCode() : 0);
         return result;
     }
 
