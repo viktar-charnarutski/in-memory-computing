142d7a9520523b731a351f6eb30c4f3956af6433
viktar-charnarutski <v.charnarutski@gmail.com>
2018-03-12 16:22:14 +0200

    Simplifying StateUs by removing state's name and code.

--- src/main/java/com/viktar/imc/coherence/vacplanner/StateUs.java
+++ src/main/java/com/viktar/imc/coherence/vacplanner/StateUs.java
@@ -1,85 +1,63 @@
 package com.viktar.imc.coherence.vacplanner;
 
 public enum StateUs {
-
-    ALABAMA("Alabama", "AL"),
-    ALASKA("Alaska", "AK"),
-    AMERICAN_SAMOA("American Samoa", "AS"),
-    ARIZONA("Arizona", "AZ"),
-    ARKANSAS("Arkansas", "AR"),
-    CALIFORNIA("California", "CA"),
-    COLORADO("Colorado", "CO"),
-    CONNECTICUT("Connecticut", "CT"),
-    DELAWARE("Delaware", "DE"),
-    DISTRICT_OF_COLUMBIA("District of Columbia", "DC"),
-    FEDERATED_STATES_OF_MICRONESIA("Federated States of Micronesia", "FM"),
-    FLORIDA("Florida", "FL"),
-    GEORGIA("Georgia", "GA"),
-    GUAM("Guam", "GU"),
-    HAWAII("Hawaii", "HI"),
-    IDAHO("Idaho", "ID"),
-    ILLINOIS("Illinois", "IL"),
-    INDIANA("Indiana", "IN"),
-    IOWA("Iowa", "IA"),
-    KANSAS("Kansas", "KS"),
-    KENTUCKY("Kentucky", "KY"),
-    LOUISIANA("Louisiana", "LA"),
-    MAINE("Maine", "ME"),
-    MARYLAND("Maryland", "MD"),
-    MARSHALL_ISLANDS("Marshall Islands", "MH"),
-    MASSACHUSETTS("Massachusetts", "MA"),
-    MICHIGAN("Michigan", "MI"),
-    MINNESOTA("Minnesota", "MN"),
-    MISSISSIPPI("Mississippi", "MS"),
-    MISSOURI("Missouri", "MO"),
-    MONTANA("Montana", "MT"),
-    NEBRASKA("Nebraska", "NE"),
-    NEVADA("Nevada", "NV"),
-    NEW_HAMPSHIRE("New Hampshire", "NH"),
-    NEW_JERSEY("New Jersey", "NJ"),
-    NEW_MEXICO("New Mexico", "NM"),
-    NEW_YORK("New York", "NY"),
-    NORTH_CAROLINA("North Carolina", "NC"),
-    NORTH_DAKOTA("North Dakota", "ND"),
-    NORTHERN_MARIANA_ISLANDS("Northern Mariana Islands", "MP"),
-    OHIO("Ohio", "OH"),
-    OKLAHOMA("Oklahoma", "OK"),
-    OREGON("Oregon", "OR"),
-    PALAU("Palau", "PW"),
-    PENNSYLVANIA("Pennsylvania", "PA"),
-    PUERTO_RICO("Puerto Rico", "PR"),
-    RHODE_ISLAND("Rhode Island", "RI"),
-    SOUTH_CAROLINA("South Carolina", "SC"),
-    SOUTH_DAKOTA("South Dakota", "SD"),
-    TENNESSEE("Tennessee", "TN"),
-    TEXAS("Texas", "TX"),
-    UTAH("Utah", "UT"),
-    VERMONT("Vermont", "VT"),
-    VIRGIN_ISLANDS("Virgin Islands", "VI"),
-    VIRGINIA("Virginia", "VA"),
-    WASHINGTON("Washington", "WA"),
-    WEST_VIRGINIA("West Virginia", "WV"),
-    WISCONSIN("Wisconsin", "WI"),
-    WYOMING("Wyoming", "WY");
-
-    private final String name;
-    private final String code;
-
-    StateUs(String name, String code) {
-        this.name = name;
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("StateUs[name=%s, code=%s]", name, code);
-    }
+    ALABAMA,
+    ALASKA,
+    AMERICAN_SAMOA,
+    ARIZONA,
+    ARKANSAS,
+    CALIFORNIA,
+    COLORADO,
+    CONNECTICUT,
+    DELAWARE,
+    DISTRICT_OF_COLUMBIA,
+    FEDERATED_STATES_OF_MICRONESIA,
+    FLORIDA,
+    GEORGIA,
+    GUAM,
+    HAWAII,
+    IDAHO,
+    ILLINOIS,
+    INDIANA,
+    IOWA,
+    KANSAS,
+    KENTUCKY,
+    LOUISIANA,
+    MAINE,
+    MARYLAND,
+    MARSHALL_ISLANDS,
+    MASSACHUSETTS,
+    MICHIGAN,
+    MINNESOTA,
+    MISSISSIPPI,
+    MISSOURI,
+    MONTANA,
+    NEBRASKA,
+    NEVADA,
+    NEW_HAMPSHIRE,
+    NEW_JERSEY,
+    NEW_MEXICO,
+    NEW_YORK,
+    NORTH_CAROLINA,
+    NORTH_DAKOTA,
+    NORTHERN_MARIANA_ISLANDS,
+    OHIO,
+    OKLAHOMA,
+    OREGON,
+    PALAU,
+    PENNSYLVANIA,
+    PUERTO_RICO,
+    RHODE_ISLAND,
+    SOUTH_CAROLINA,
+    SOUTH_DAKOTA,
+    TENNESSEE,
+    TEXAS,
+    UTAH,
+    VERMONT,
+    VIRGIN_ISLANDS,
+    VIRGINIA,
+    WASHINGTON,
+    WEST_VIRGINIA,
+    WISCONSIN,
+    WYOMING
 }
